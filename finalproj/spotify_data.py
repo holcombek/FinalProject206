43357d01a2eef7a665bcd23a57e0eae667c06483
Kristina Holcombe <kholc@umich.edu>
2022-04-11 11:26:49 -0400

    now adding 25 items at a time

---
+++ finalproj/spotify_data.py
@@ -0,0 +1,25 @@
+import sqlite3
+import os
+import requests
+from bs4 import BeautifulSoup
+import json
+
+# This file gets data from the Spotify API for the top 100 billboard songs
+# could look at genre of song?
+# most popular genre
+# does most common genre on chart correlate to stocks
+# most popular genre in general correlate? pop?
+
+# could look at followers of artist?
+
+# popularity of a song?
+
+#for spotify
+#def get_data_spotify:
+    #client_id = 'CLIENT ID'\
+    # #client_secret  = '' 
+    # AUTHROIZE = 'https://accounts.spotify.com/authorize"
+    #redirect_url = 'http://127.0.0.1:5500/index.html'
+# table for spotify needs to include song title artist and smth else?
+
+# visualisations of chosen data 
