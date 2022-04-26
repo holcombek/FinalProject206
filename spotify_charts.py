275d7cccb82947d07eb7f99b28c34f14c6ee7044
Kristina Holcombe <kholc@umich.edu>
2022-04-26 15:30:18 -0400

    documentation added, complete!

--- spotify_charts.py
+++ spotify_charts.py
@@ -4,21 +4,31 @@ from bs4 import BeautifulSoup
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-#This file parses through html files saved from the Spotify Charts (older) website (https://spotifycharts.com/regional)
-# and saves all data into a dictionary for each week, then uses those dictionaries to cross reference with Billboard
-# Hot 100 database to get streaming info for each song on both lists
+#This file parses through html files saved from the Spotify Charts 
+# (older) website (https://spotifycharts.com/regional)
+# and saves all data into a dictionary for each week, then uses those dictionaries 
+# to cross reference with Billboard Hot 100 database 
+# to get streaming info for each song on both lists
 
-#Sets up connection with database
 def setUpDatabase(db_name):
+    '''
+    Takes in database name; returns cursor and connection objects.
+    Used in main to set up database.
+    '''
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_name)
     cur = conn.cursor()
     return cur, conn
 
-# gathers data from spotifycharts html file and put into dictionary
-# html files cover the past four weeks (3-17, 3-24, 3-31, 4-07)
-# each html page has the 200 songs most streamed in that week on Spotify
-def get_data():
+def get_charts_data():
+    '''
+    Takes in nothing; returns list of dictionaries with song rank
+     as key and song title, artist, and number of streams as values
+     for each week of Spotify top 200 streamed songs.
+    Scrapes SpotifyCharts HTML files of given weeks below for
+     song data using BeautifulSoup objects.
+    '''
+    # list of last 4 weeks of Spotify Charts data, with date as beginning of week
     file_lst = ['wk3.17spotify.html', 'wk3.24spotify.html', 'wk3.31spotify.html', 'wk4.7spotify.html']
     base_path = os.path.abspath(os.path.dirname(__file__))
     #all_lst holds all four dictionaries (one for each week)
@@ -46,29 +56,35 @@ def get_data():
                 # getting title and artist and stripping any excess characters
                 title = song.find('strong').text.strip()
                 artist = song.find('span').text.strip()
-                stuff = top_song_dict.get(j)
+                curr_stuff = top_song_dict.get(j)
                 # adding each song to the top_songs dict
-                top_song_dict[j] = (stuff, title, artist)
+                top_song_dict[j] = (curr_stuff, title, artist)
                 j += 1
             all_lst.append(top_song_dict)
     return all_lst
 
-# takes each week's dictionary to cross reference with billboard list for corresponding week and gather streaming data
-def database(dictr, j, names, start, cur,conn):
+def database(dictr, week, name, start, cur, conn):
+    '''
+    Takes in given weeks dictionary of Spotify Charts data, week variable, 
+     name of table to be created, start variable, cursor, connection; returns nothing.
+    Creates Spotify Streams table for given week
+     with Billboard rank (song_id) and number of streams
+     by looping through Spotify Charts dict 25 items at a time.
+    '''
+    # comment out if need to reset data collection
     #cur.execute('DROP TABLE IF EXISTS ' + names)
-    # billboard tables in database
     # week 1 is earliest week, week 4 most recent
     name_lst = ['Billboard_week_1', 'Billboard_week_2', 'Billboard_week_3', 'Billboard_week_4']
-    cur.execute('CREATE TABLE IF NOT EXISTS ' + names + ' (song_id INTEGER PRIMARY KEY, streams INTEGER)')
+    cur.execute('CREATE TABLE IF NOT EXISTS ' + name + ' (song_id INTEGER PRIMARY KEY, streams INTEGER)')
     conn.commit()
-    # getting all song titles only from billboard to cross reference
-    cur.execute(f'SELECT song_title FROM ' + name_lst[j])
+    # getting song titles ONLY from billboard to cross reference
+    cur.execute(f'SELECT song_title FROM ' + name_lst[week])
     billboard_songs = cur.fetchall()
     new_billboard = []
     current = []
     streaming = []
+    # adds billboard song titles as a string to new list
     for song in billboard_songs:
-        # adds billboard song titles as a string to new list
         new_billboard.append(song[0])
     for item in range(len(dictr)):
         # adds current spotify charts song to list as a stripped down string
@@ -85,62 +101,90 @@ def database(dictr, j, names, start, cur,conn):
         if to_cross in current:
             # gets index to reference for streams
             position = current.index(new_billboard[i])
-            cur.execute(f'SELECT * FROM {name_lst[j]} WHERE song_title = \'{current[position]}\' ')
+            cur.execute(f'SELECT * FROM {name_lst[week]} WHERE song_title = \'{current[position]}\' ')
             song_data = cur.fetchall()
             # accounts for potential empty data
             if song_data == []:
                 continue
             else:
-                # gets id from Billboard chart, to cross reference later for song_title
+                # gets song_id from Billboard chart, to cross reference laterin file for song title and artist
                 id = song_data[0][0]
-                cur.execute(f'INSERT INTO {names} (song_id, streams) VALUES(?,?)', (id, streaming[position]))
+                cur.execute(f'INSERT INTO {name} (song_id, streams) VALUES(?,?)', (id, streaming[position]))
                 conn.commit()
     conn.commit()
 
-#Using JOIN on billboard and spotify tables; match song_id's so that we match song to number of streams
-def joining_billboard_spotify_tables(i, cur, conn):
-    #cur.execute(f'DROP TABLE IF EXISTS billboardXspotify_wk{i}')
-    cur.execute(f'''CREATE TABLE billboardXspotify_wk{i} AS 
-                    SELECT Billboard_week_{i}.song_id AS 'billboard_rank_id',
-                    Billboard_week_{i}.artist, Billboard_week_{i}.song_title, spotify_streams_week_{i}.streams 
-                    FROM Billboard_week_{i} JOIN spotify_streams_week_{i} 
-                    ON Billboard_week_{i}.song_id = spotify_streams_week_{i}.song_id
-                    ORDER BY spotify_streams_week_{i}.streams DESC''')
+def joining_billboard_spotify_tables(week, cur, conn):
+    ''' 
+    Takes in week variable, cursor, connection; returns nothing.
+    Uses SQL query with JOIN on song_ids to create a new, combined table of
+     song info from Billboard table and Spotify table for given week 
+     (with billboard rank, song title, artist, and spotify streams).
+    '''
+    # comment out unless needed to reset data in tables
+    #cur.execute(f'DROP TABLE IF EXISTS billboardXspotify_wk{week}')
+    cur.execute(f'''CREATE TABLE billboardXspotify_wk{week} AS 
+                    SELECT Billboard_week_{week}.song_id AS 'billboard_rank_id',
+                    Billboard_week_{week}.song_title, Billboard_week_{week}.artist, spotify_streams_week_{week}.streams 
+                    FROM Billboard_week_{week} JOIN spotify_streams_week_{week} 
+                    ON Billboard_week_{week}.song_id = spotify_streams_week_{week}.song_id
+                    ORDER BY spotify_streams_week_{week}.streams DESC''')
+    conn.commit()
 
-def visualisation(cur, conn):
-    #finding max streams from each week
+def streams_visualisation(cur, conn):
+    '''
+    Takes in cursor, connection; returns nothing.
+    Finds MAX numbers of streams using SQL query from
+     joined BillboardxSpotify table for each week
+     and creates a scatter graph using Seaborn showing the 
+     change over last four weeks.
+    '''
     max_streams = []
     for i in range(1,5):
-        cur.execute(f'SELECT MAX(streams), song_title FROM billboardXspotify_wk{i}')
+        # finds MAX streams for given week
+        cur.execute(f'SELECT MAX(streams) FROM billboardXspotify_wk{i}')
+        conn.commit()
         info = cur.fetchall()
         max_streams.append(info[0][0])
+    sns.set_style('darkgrid')
+    # creates plot and labels with Seaborn
     print(max_streams)
-    plt.figure(figsize=(10,10))
-    plt.title("Billboard Top Song Number of Streams Over Past 4 Weeks")
-    plt.xlabel('Weeks')
-    plt.ylabel('Number of Streams (in tens of millions)')
-    streamplot = sns.lineplot(data=max_streams, markers = True, dashes= True)
+    streamplot = sns.relplot(data=max_streams, color = 'magenta')
+    streamplot.set(xlabel = 'Weeks', ylabel = 'Number of Streams (in tens of millions)', title = 'Billboard Top Song Number of Streams Over Past 4 Weeks')
     plt.show()
 
 def main():
-    #Setting up connection to database
+    '''
+    Main function of file.
+    Sets up database and sets up list of dictionaries with song info.
+    Adds song information for each week to corresponding table
+     25 items at a time (for each of the four weeks) with changing start value.
+    Joins tables in database to create new tables with 
+     billboard rank and number of spotify streams.
+    Shows visualization.
+    '''
     cur,conn = setUpDatabase('final.db')
 
-    #List of playlist data (each one has 200 songs)
-    playlist_lst = get_data()
+    # list of playlist dictionaries (each one has 200 songs)
+    playlist_lst = get_charts_data()
 
-    #Set up names, start range
+    # set up names of tables, start range
     name_lst = ['spotify_streams_week_1' , 'spotify_streams_week_2', 'spotify_streams_week_3', 'spotify_streams_week_4']
-    start = 75
+    
+    #start values are 0, 25, 50, 75; change start value each time you run file
+    # to next start value (four times in total) beginning with 0
+    start = 0
 
-    #Run 4 times to go through all 100 songs of each playlist
+    # comment out if already created
+    # run 4 times to go through all 100 songs of each playlist
     #for i in range(4):
         #database(playlist_lst[i], i, name_lst[i], start, cur, conn)
     
+    # comment out if already created
     #for i in range(1,5):
         #joining_billboard_spotify_tables(i, cur, conn)
     
-    visualisation(cur, conn)
+    # comment out if don't want to see visualization
+    # streams_visualisation(cur, conn)
 
 
 
