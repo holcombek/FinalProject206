9df4db28e26d07cf24f6353c6939a88989759f7f
AaronSantilli <98405405+AaronSantilli@users.noreply.github.com>
2022-04-26 20:36:19 -0400

    Add files via upload
    'Comments added to run the code and cleaned up -- All Done!'

--- Yahoo Finance Data.py
+++ Yahoo Finance Data.py
@@ -5,21 +5,14 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import yfinance as yf
+from zipfile import ZipFile
 
 
-
-#api = 'https://yfapi.net/v8/finance/chart/SPOT?range=1mo&region=US&interval=1d&lang=en&events=div%2Csplit'
-
-#api2 = 'https://yfapi.net/v6/finance/quote/SPOT'
-
-#api_key = 'jr6crJlIJh1uTSmQ0tHsM7DJWCjfsPsu5xCVCBbo'
-
-#Get Yahoo Finance Spotify Data from API
+#Get Yahoo Finance Spotify Data from APIs
 def get_Spotify_Price():
     price_List = []
     spot = yf.Ticker('SPOT')
     resp = spot.history(period = '1mo', interval = '1d')
-    #resp = requests.get(api, headers = 'X-API-KEY : jr6crJlIJh1uTSmQ0tHsM7DJWCjfsPsu5xCVCBbo', params = {'range' : '1mo', 'region' : 'US', 'interval' : '1d', 'language' : 'en', 'ticker' : 'SPOT'})
     for i in resp['Close']:
         price_List.append(i)
 
@@ -28,7 +21,6 @@ def get_Spotify_Price():
 #Get Trading Volume of Spotify from API
 def get_Spotify_Volume():
     volume_list = []
-    #response = requests.get(api2, params = {'region' : 'US', 'lang' : 'en', 'symbols' : 'SPOT'})
     spot = yf.Ticker('SPOT')
     response = spot.history(period = '1mo', interval = '1d')
     print(response)
@@ -48,20 +40,19 @@ def get_Variance():
 
     return variance_list
 
+#Connect to Database for Data Retrieval
 def setUpDatabase(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_name)
     cur = conn.cursor()
     return cur, conn
 
+#Retrieve Number of Streams from the Database
 def retrieve_Streams(cur, conn):
     list1 = []
     list2 = []
     list3 = []
     list4 = []
-    #connection = setUpDatabase('final.db')
-    #conn = sqlite3.connect('final.db')
-    #cursor = connection.cursor()
     cur.execute('''SELECT streams FROM spotify_streams_week_1''')
     result1 = cur.fetchall()
     cur.execute('''SELECT streams FROM spotify_streams_week_2''')
@@ -109,8 +100,6 @@ def visualize_Variance():
     variance_graph = plt.xticks([2, 4, 6, 8, 10, 12, 14, 16, 18, 20])
     variance_graph = sns.lineplot(data = df, x = df.index, y = 'Variance', color = 'red', linewidth = 2.5)
     variance_graph.set(xlabel = 'Day', ylabel = 'Variance', title = 'Day-to-Day Variance of Spotify Stock Price Over 4 Weeks')
-
-   # plt.title('Spotify Daily Stock Price Variance over 4 Weeks')
     plt.show()
     
     return variance_graph
@@ -123,12 +112,11 @@ def visualize_Volume():
     sns.set_style('darkgrid')
     volume_graph = sns.lineplot(data = response, x = 'Date', y = 'Volume', color = 'gold', linewidth = 2.5)
     volume_graph.set(xlabel = 'Day', ylabel = 'Volume', title = "Spotify Trading Volume")
-
-    #plt.title('Price-to-Earnings Ratio over 4 Weeks')
     plt.show()
 
     return volume_graph
 
+#Visualize a linear regression plot for the two variables of Streams and Stock Price
 def visualize_correlation(cur, conn):
     spot = yf.Ticker('SPOT')
     data1 = retrieve_Streams(cur, conn)
@@ -145,20 +133,37 @@ def visualize_correlation(cur, conn):
     plt.figure(figsize=(10,16))
     visualizeCorrelation = sns.regplot(data = final_df, color = 'purple',
      x = 'Weekly Total Spotify Streams', 
-     y = 'Weekly Spotify Stock Price')
+     y = 'Weekly Spotify Stock Price',
+     )
+    visualizeCorrelation.set(title = "Linear Regression of Total Top Song Streams and Weekly Stock Price")
 
     plt.show()
 
     return visualizeCorrelation
 
+#Write Calculations to Text File which can then be zipped
+def write_to_text(calculation1, calculation2, calculation3, calculation4):
+   file_name = 'calculations.txt'
+   #full_path = os.path.join(save_path, file_name)
+   with open(file_name, 'w') as f:
+    f.writelines(' Spotify Price: ' + str(calculation1) + '\n')
+    f.writelines(' Spotify Trading Volume: ' + str(calculation2) + '\n')
+    f.writelines(' Spotify Stock Price Variance: ' + str(calculation3) + '\n')
+    f.writelines(' Stream Data from Database: ' + str((calculation4)) + '\n')
+   
+    f.close()
+
+
+#Main function to call the code
 def main():
     cur, conn = setUpDatabase('final.db')
-    get_Spotify_Price()
-    get_Spotify_Volume()
-    get_Variance()
+    calculation1 = get_Spotify_Price()
+    calculation2 = get_Spotify_Volume()
+    calculation3 = get_Variance()
     visualize_Variance()
     visualize_Volume()
-    retrieve_Streams(cur, conn)
+    calculation4 =  retrieve_Streams(cur, conn)
     visualize_correlation(cur, conn)
+    write_to_text(calculation1, calculation2, calculation3, calculation4)
 
 main()
